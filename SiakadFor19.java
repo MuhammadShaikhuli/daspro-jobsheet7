1f4f6b3dd4f5074480fdf358bd96af296888683c
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2025-11-07 09:01:13 +0700

    Ini adalah program menentukan nilai tertinggi dan terendah

---
+++ SiakadFor19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SiakadFor19 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0, terrendah = 100;
+
+        for (int i = 1; i <= 10; i++){
+            System.out.print("Masukkan Nilai Manasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi){
+                tertinggi = nilai;
+            }
+            if (nilai < terrendah){
+                terrendah = nilai;
+            }
+        }
+    System.out.print("Nilai Tertinggi: " +tertinggi +"\n");
+    System.out.print("Nilai Terrendah: " +terrendah);
+    }
+}
